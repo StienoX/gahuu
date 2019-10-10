6a9590aceba6ad5f173f244aef3b99eb1e1dc6a8
StienoX <stieno@ArchS-1.localdomain>
2019-10-10 15:25:35 +0200

    Added comment for AI functions

--- src/Ai.hs
+++ src/Ai.hs
@@ -6,9 +6,11 @@ data AI_type = AI1 | AI2 --Possible more types if needed
 data AI aiType x y = MkAI AI_type Float Float Hitbox
   deriving (Eq)
 
+--Processes the behavior for the provided ai
 processAI :: AI -> Float -> Player -> AI
 processAI (MkAI AI1 x y) speed player = undefined
 processAI (MkAI AI2 x y) speed player = undefined
 
+--Processes all ai behavior
 updateAI :: [AI] -> Float -> Player -> [AI]
 updateAI xs speed player = map (processAI speed player) xs
