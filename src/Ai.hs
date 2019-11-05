50886042a076fc9f7b3c987a84b3c422d9770e3a
StienoX <stieno@ArchS-1.localdomain>
2019-11-05 09:45:40 +0100

    Bugfix missing parameter in processAI function in Ai.hs

--- src/Ai.hs
+++ src/Ai.hs
@@ -7,14 +7,15 @@ import Util
 
 --Processes the behavior for the provided ai
 processAI :: AI -> Float -> Player -> AI
-processAI (MkAI AI1 aiPos) speed Player {pos} | (aiPos - pos) < (0,0) = MkAI AI1 (aiPos - (speed,0))
-                                                          | otherwise             = MKAI AI1 (aiPos + (speed,0))
+processAI (MkAI AI1 aiPos hitbox) speed player  | (aiPos - (pos player)) < (0,0) = MkAI AI1 (aiPos - (speed,0)) hitbox
+                                                | otherwise                      = MkAI AI1 (aiPos + (speed,0)) hitbox
 
-processAI (MkAI AI2 aiPos) speed Player {pos} = MKAI AI2 (speed * (pos - aiPos) + aiPos)
+processAI (MkAI AI2 aiPos hitbox) speed player                                   = MkAI AI2 ((speed,speed) * ((pos player) - aiPos) + aiPos) hitbox
 
-processAI (MkAI AI3 aiPos) speed Player {pos} | (aiPos - pos) < (0,0) = MkAI AI1 (aiPos + (speed,0))
-                                                          | otherwise             = MKAI AI1 (aiPos - (speed,0))
+processAI (MkAI AI3 aiPos hitbox) speed player  | (aiPos - (pos player)) < (0,0) = MkAI AI1 (aiPos + (speed,0)) hitbox
+                                                | otherwise                      = MkAI AI1 (aiPos - (speed,0)) hitbox
 
 --Processes all ai behavior
 updateAI :: [AI] -> Float -> Player -> [AI]
-updateAI xs speed player = map (processAI speed player) xs
+updateAI xs speed player = map f xs
+  where f x = processAI x speed player
